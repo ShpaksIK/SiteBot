953265a1a2ad69b648fed8b38fddc3886fb0d03b
ShpaksIK <masimiosipcuk2020@gmail.com>
2024-08-21 23:49:30 +0300

    add command bot

--- cogs/general.py
+++ cogs/general.py
@@ -4,13 +4,13 @@ import discord
 from discord.ext import commands
 
 sys.path.append("..")
-from main import BOT_PREFIX, BOT_SITE, db_con
+from main import BOT_PREFIX, BOT_SITE, BOT_NAME, db_con
 
 
 class GeneralCog(commands.Cog):
     """ Модуль, содержащий основные команды бота. """
     def __init__(self, bot):
-        self.bot = bot
+        self.boter = bot
         print(f"Module GeneralCog is loaded!")
 
 
@@ -64,6 +64,24 @@ class GeneralCog(commands.Cog):
                 emb1.add_field(name = f"", value=f"Ещё больше информации о командах вы можете найти на сайте бота: {BOT_SITE}commands", inline=False)
             await ctx.send(embed = emb1)
 
+    @commands.command()
+    async def bot(self, ctx):
+        joined_date = self.boter.get_guild(1176528473112711229).get_member(self.boter.user.id).joined_at.strftime('%d.%m.%Y, %H:%M')
+        emb1 = discord.Embed(title=f"Информация о {BOT_NAME}", color=0xffffff)
+        emb1.add_field(name="Ник", value=f"<@{self.boter.user.id}> | `{self.boter.user.name}`", inline=True)
+        emb1.add_field(name="Айди", value=f"`{self.boter.user.id}`", inline=True)
+        emb1.add_field(name="Префикс", value=f"`{BOT_PREFIX}`", inline=True)
+        emb1.add_field(name="Бот был создан", value=f'{self.boter.user.created_at.strftime("%d.%m.%Y, %H:%M")}', inline=True)
+        emb1.add_field(name="Присоединился к серверу", value=f'{joined_date}', inline=True)
+        emb1.add_field(name = f"Ссылки", value=f"[Сайт бота](http://localhost:3000/)\n[Панель управления](http://localhost:3000/servers)", inline=False)
+        emb1.add_field(name = f"", value=f"Ыыыыыы наш бот очень крутооой ыываыаыаыаы!!!!", inline=False)
+        emb1.set_thumbnail(url=self.boter.user.avatar)
+        await ctx.send(embed = emb1)
+
+    @commands.command()
+    async def server(self, ctx):
+        pass
+    
     
 async def setup(bot):
     await bot.add_cog(GeneralCog(bot))
